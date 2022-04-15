8b3dd251534e81ea9f2180af1b59b38f33833bff
CristianMiguelMB <cristian_enano_99@hotmail.com>
2022-04-14 22:24:08 -0500

    Avance del backEnd

---
+++ NodeTPM/src/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const cors = require('cors');
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(require('./routes/favoritos'));
+
+module.exports = app;
