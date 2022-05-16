85dab342b6fbf261b333685dbf7692d984367586
CristianMiguelMB <cristian_enano_99@hotmail.com>
2022-05-15 23:48:27 -0500

    Esqueleto de Home Page hecha

--- ProyectoTPM/lib/src/home.dart
+++ ProyectoTPM/lib/src/home.dart
@@ -10,8 +10,275 @@ class Home extends StatefulWidget{
 class _Home extends State<Home>{
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Home'),
+    return ListView(
+      scrollDirection: Axis.vertical,
+      children:<Widget>[
+          Container(
+            alignment: Alignment.centerLeft,
+            margin: const EdgeInsets.only(left:16, right: 10, top: 30),
+            child: Text(
+                "Descubre\nMichoacán",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                   fontSize: 28,
+                   fontWeight: FontWeight.w500,
+                   color: Colors.black87.withOpacity(0.7),
+                ),
+            ),
+          ),
+          Container(
+              height: 60,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  Container(
+                    margin:  const EdgeInsets.only(left: 16, top: 10),
+                    width: 120,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      color: Colors.black87,
+                      borderRadius: BorderRadius.circular(10),
+                      image: const DecorationImage(
+                        image: AssetImage ('assets/Hotel.png'),
+                        fit: BoxFit.cover,
+                        opacity: 0.5,
+                      ),
+                    ),
+                    child: TextButton(
+                      onPressed: (){},
+                      child: const Text(
+                          "Hoteles",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin:  const EdgeInsets.only(left: 16, top: 10),
+                    width: 120,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(10),
+                      image: const DecorationImage(
+                        image: AssetImage ('assets/Restaurante.png'),
+                        fit: BoxFit.cover,
+                        opacity: 0.5,
+                      ),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        "Restaurantes",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin:  const EdgeInsets.only(left: 16, top: 10),
+                    width: 120,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(10),
+                      image: const DecorationImage(
+                        image: AssetImage ('assets/Tour.png'),
+                        fit: BoxFit.cover,
+                        opacity: 0.5,
+                      ),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        "Tours",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin:  const EdgeInsets.only(left: 16, top: 10),
+                    width: 120,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(10),
+                      image: const DecorationImage(
+                        image: AssetImage ('assets/Viajes.png'),
+                        fit: BoxFit.cover,
+                        opacity: 0.5,
+                      ),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        "Viajes",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin:  const EdgeInsets.only(left: 16, right:16, top: 10),
+                    width: 120,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(10),
+                      image: const DecorationImage(
+                        image: AssetImage ('assets/Paquete.png'),
+                        fit: BoxFit.cover,
+                        opacity: 0.5,
+                      ),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        "Paquetes",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+          ),
+
+          Container(
+            height: 400,
+            width: Theme.of(context).textTheme.bodyText1!.fontSize! * 30,
+            margin:  const EdgeInsets.only(left: 16, right:16, top: 25, bottom: 25),
+            decoration: BoxDecoration(
+                color: Colors.black87,  //PARA PROBAR CONTAINER
+                borderRadius: BorderRadius.circular(10.0),
+                image: const DecorationImage(
+                  image: AssetImage ('assets/Tirolesa.png'),
+                  //NetworkImage("https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/7a/ea/98/caption.jpg?w=500&h=400&s=1"),
+                  fit: BoxFit.cover,
+                ),
+                boxShadow: const[
+                  BoxShadow(
+                    //SOMBRA
+                    color: Color(0xffA4A4A4),
+                    offset: Offset(1.0, 5.0),
+                    blurRadius: 3.0,
+                  ),
+                ]
+            ),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.centerLeft,
+                  margin: const EdgeInsets.only(left: 16, right:16, top: 25, bottom: 4),
+                  child: const Text(
+                    "¡Actividades\npara tu viaje!",
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  margin: const EdgeInsets.only(left: 16, right:16),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.only(left: 30, right:30),
+                      shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(8.0),
+                      ),
+                      primary: Colors.white,
+                      onPrimary: Colors.black12,
+                      // side: BorderSide(color: Colors.red, width: 1),
+                    ),
+                    onPressed: (){},
+                    child: const Text(
+                      'Experiencias',
+                      style: TextStyle(
+                        color: Colors.black87,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+        Container(
+          height: 400,
+          width: Theme.of(context).textTheme.bodyText1!.fontSize! * 30,
+          margin:  const EdgeInsets.only(left: 16, right:16, top: 18, bottom: 25),
+          decoration: BoxDecoration(
+              color: Colors.black,  //PARA PROBAR CONTAINER
+              borderRadius: BorderRadius.circular(10.0),
+              image: const DecorationImage(
+                image: AssetImage ('assets/Carnitas.png'),
+                // NetworkImage("https://cdn2.cocinadelirante.com/sites/default/files/styles/gallerie/public/images/2020/09/como-se-hacen-las-carnitas-facil.jpg"),
+                fit: BoxFit.cover,
+                opacity: 0.7,
+              ),
+              boxShadow: const[
+                BoxShadow(
+                  //SOMBRA
+                  color: Color(0xffA4A4A4),
+                  offset: Offset(1.0, 5.0),
+                  blurRadius: 3.0,
+                ),
+              ]
+          ),
+          child: Column(
+            children: <Widget>[
+              Container(
+                alignment: Alignment.centerLeft,
+                margin: const EdgeInsets.only(left: 16, right:16, top: 25, bottom: 4),
+                child: const Text(
+                  "Disfruta de\nnuestra gastronomía",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                margin: const EdgeInsets.only(left: 16, right:16),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.only(left: 30, right:30),
+                    shape: new RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(8.0),
+                    ),
+                    primary: Colors.white,
+                    onPrimary: Colors.black12,
+                    // side: BorderSide(color: Colors.red, width: 1),
+                  ),
+                  onPressed: (){},
+                  child: const Text(
+                    'Experiencias',
+                    style: TextStyle(
+                      color: Colors.black87,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
--- ProyectoTPM/lib/src/perfil.dart
+++ ProyectoTPM/lib/src/perfil.dart
@@ -9,111 +9,156 @@ class Perfil extends StatefulWidget{
 }
 
 class _Perfil extends State<Perfil>{
+  final _emailInputTextController = TextEditingController();
+  final _passwordInputTextController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            margin: const EdgeInsets.only(bottom: 40),
-            width: 90,
-            height: 90,
-            // color: const Color.fromRGBO(32, 32, 32, 0.7),
-            decoration: BoxDecoration(
-              color: Color.fromRGBO(32, 32, 32, 0.7),
-              borderRadius: BorderRadius.circular(50),
-            ),
-            child: const Icon(
-                Icons.card_travel,
-              color: Colors.white,
-              size: 60,
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(left:12, right: 12, bottom: 10),
-            alignment: Alignment.centerLeft,
-            child: const Text(
-                "Correo Electrónico",
-              style: TextStyle(
-                fontWeight: FontWeight.w400,
-                fontSize: 15,
-              ),
-            ),
-          ),
-          Container(
-            width: double.infinity,
-            height: 40,
-            margin: const EdgeInsets.only(left:12, right: 12),
-            decoration: BoxDecoration(
-              color: Color.fromRGBO(93, 93, 93, 0.1), borderRadius: BorderRadius.circular(50),
-            ),
-            child: const Center(
-              child: TextField(
-                textAlign: TextAlign.center,
-                decoration: InputDecoration(
-                  hintText: '...Correo...',
-                  border: InputBorder.none,
-                ),
-              ),
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(left:12, right: 12, bottom: 10, top: 25),
-            alignment: Alignment.centerLeft,
-            child: const Text(
-              "Contraseña",
-              style: TextStyle(
-                fontWeight: FontWeight.w400,
-                fontSize: 15,
-              ),
-            ),
-          ),
-          Container(
-            width: double.infinity,
-            height: 40,
-            margin: const EdgeInsets.only(left:12, right: 12),
-            decoration: BoxDecoration(
-              color: Color.fromRGBO(93, 93, 93, 0.1), borderRadius: BorderRadius.circular(50),
-            ),
-            child: const Center(
-              child: TextField(
-                textAlign: TextAlign.center,
-                decoration: InputDecoration(
-                  hintText: '...Contraseña...',
-                  border: InputBorder.none,
-                ),
-              ),
-            ),
-          ),
-          Row(
+      child: Expanded(
+        child: SingleChildScrollView(
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
             children: <Widget>[
+
               Container(
-                width: Theme.of(context).textTheme.bodyText1!.fontSize! * 10.5,
-                margin: const EdgeInsets.only(left:0, right: 6, top: 20),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    shape: new RoundedRectangleBorder(
-                      borderRadius: new BorderRadius.circular(10.0),
-                    ),
-                    primary: Colors.white54,
-                    onPrimary: Colors.white,
-                    // side: BorderSide(color: Colors.red, width: 1),
+                margin: const EdgeInsets.only(bottom: 40),
+                width: 90,
+                height: 90,
+                // color: const Color.fromRGBO(32, 32, 32, 0.7),
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(32, 32, 32, 0.7),
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: const Icon(
+                  Icons.card_travel,
+                  color: Colors.white,
+                  size: 60,
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(left:12, right: 12, bottom: 10),
+                alignment: Alignment.centerLeft,
+                child: const Text(
+                  "Correo Electrónico",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 15,
                   ),
-                  onPressed: (){},
-                  child: const Text(
-                    'Cancelar',
-                    style: TextStyle(
-                      color: Colors.black87,
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                height: 40,
+                margin: const EdgeInsets.only(left:12, right: 12),
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(93, 93, 93, 0.1), borderRadius: BorderRadius.circular(50),
+                ),
+                child: const Center(
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      hintText: '...Correo...',
+                      border: InputBorder.none,
                     ),
                   ),
                 ),
               ),
               Container(
-                width: Theme.of(context).textTheme.bodyText1!.fontSize! * 14.5,
-                margin: const EdgeInsets.only(left:6, right: 0, top: 20),
+                margin: const EdgeInsets.only(left:12, right: 12, bottom: 10, top: 25),
+                alignment: Alignment.centerLeft,
+                child: const Text(
+                  "Contraseña",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 15,
+                  ),
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                height: 40,
+                margin: const EdgeInsets.only(left:12, right: 12),
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(93, 93, 93, 0.1), borderRadius: BorderRadius.circular(50),
+                ),
+                child: const Center(
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      hintText: '...Contraseña...',
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Container(
+                    width: Theme.of(context).textTheme.bodyText1!.fontSize! * 10.5,
+                    margin: const EdgeInsets.only(left:0, right: 6, top: 20),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: new RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(10.0),
+                        ),
+                        primary: Colors.white54,
+                        onPrimary: Colors.white,
+                        // side: BorderSide(color: Colors.red, width: 1),
+                      ),
+                      onPressed: (){},
+                      child: const Text(
+                        'Cancelar',
+                        style: TextStyle(
+                          color: Colors.black87,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: Theme.of(context).textTheme.bodyText1!.fontSize! * 14.5,
+                    margin: const EdgeInsets.only(left:6, right: 0, top: 20),
+
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: new RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(10.0),
+                        ),
+                        primary: Colors.pinkAccent,
+                        onPrimary: Colors.white,
+                        // side: BorderSide(color: Colors.red, width: 1),
+                      ),
+                      onPressed: (){},
+                      child: const Text(
+                        'Iniciar Sesión',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+
+                ],
+              ),
+              TextButton(
+                onPressed: (){},
+                child: const Text(
+                  'Olvide mi contraseña',
+                  style: TextStyle(
+                    color: Colors.black87,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+              const Divider(
+                color: Colors.black38,
+              ),
+
+              Container(
+                width: Theme.of(context).textTheme.bodyText1!.fontSize! * 30,
+                margin: const EdgeInsets.only(left:16, right: 16, top: 10),
 
                 child: ElevatedButton(
                   style: ElevatedButton.styleFrom(
@@ -126,7 +171,7 @@ class _Perfil extends State<Perfil>{
                   ),
                   onPressed: (){},
                   child: const Text(
-                    'Iniciar Sesión',
+                    'Registrate',
                     style: TextStyle(
                       color: Colors.white,
                     ),
@@ -136,46 +181,9 @@ class _Perfil extends State<Perfil>{
 
             ],
           ),
-          TextButton(
-              onPressed: (){},
-              child: const Text(
-                  'Olvide mi contraseña',
-                  style: TextStyle(
-                    color: Colors.black87,
-                    fontSize: 14,
-                    fontWeight: FontWeight.w400,
-                  ),
-              ),
-          ),
-          const Divider(
-              color: Colors.black38,
-          ),
+        ),
+      ),
 
-          Container(
-            width: Theme.of(context).textTheme.bodyText1!.fontSize! * 30,
-            margin: const EdgeInsets.only(left:16, right: 16, top: 10),
-
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                shape: new RoundedRectangleBorder(
-                  borderRadius: new BorderRadius.circular(10.0),
-                ),
-                primary: Colors.pinkAccent,
-                onPrimary: Colors.white,
-                // side: BorderSide(color: Colors.red, width: 1),
-              ),
-              onPressed: (){},
-              child: const Text(
-                'Registrate',
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ),
-
-        ],
-      ) ,
     );
   }
 }
